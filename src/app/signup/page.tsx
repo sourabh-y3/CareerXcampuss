1c680f8e714ea45e0a02e6d37244b4808dd9397e
Sourabh Yadav <sourabh3y@gmail.com>
2026-01-11 18:44:05 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { PlaceHolderImages } from '@/lib/placeholder-images'
