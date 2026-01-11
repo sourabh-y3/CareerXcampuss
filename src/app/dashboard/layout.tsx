e2ebc18be3355c6d2d7d8b80cd56fd8951ed983d
Sourabh Yadav <sourabh3y@gmail.com>
2026-01-11 19:03:19 +0000

    overlapping of sidebar and pages (_for element <SidebarContent>_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         </Sidebar>
         <SidebarInset>
           <Header />
-          <main className="p-4 sm:px-6 sm:py-0">{children}</main>
+          <main className="p-4 sm:p-6">{children}</main>
         </SidebarInset>
       </div>
     </SidebarProvider>
